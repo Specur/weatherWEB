3e65ac94118046e4658bd2158f5ce29841d76a0b
Specur <mmmstec@o2.pl>
2017-04-08 23:08:27 +0200

    dodanie testow do Temperature.java

--- src/com/packt/webstore/controller/HomeController.java
+++ src/com/packt/webstore/controller/HomeController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//mapowanie na add 
-//ten fragment jest do testow
 @Controller
 public class HomeController {
 	@RequestMapping("/add")
